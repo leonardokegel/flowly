c4a686869aa5827ddb2e1f21946d9fda4e18d74f
Pedro Freitas <pedrinfreitas@gmail.com>
2023-04-27 21:03:51 -0300

    fix: testes unitarios

--- src/app/pages/login/reset-password/reset-password.component.spec.ts
+++ src/app/pages/login/reset-password/reset-password.component.spec.ts
@@ -1,25 +1,52 @@
 // import { NO_ERRORS_SCHEMA } from '@angular/core';
 // import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+// import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 // import { ResetPasswordComponent } from './reset-password.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { ResetPasswordComponent } from './reset-password.component';
+
 
 // describe('ResetPasswordComponent', () => {
 //   let component: ResetPasswordComponent;
 //   let fixture: ComponentFixture<ResetPasswordComponent>;
 
-//   beforeEach(() => {
+//     beforeEach(() => {
 //     TestBed.configureTestingModule({
 //       schemas: [NO_ERRORS_SCHEMA],
 //       declarations: [ResetPasswordComponent],
 //       imports: [ReactiveFormsModule, FormsModule],
+//       providers: [ FormBuilder ]
 //     }).compileComponents();
 
-//     fixture = TestBed.createComponent(ResetPasswordComponent);
-//     component = fixture.componentInstance;
-//   });
+//   // const formBuilder: FormBuilder;
+
+//   // const component: ResetPasswordComponent = new ResetPasswordComponent();
 
 //   it('should create', () => {
 //     expect(component).toBeTruthy();
 //   });
 // });
+
+describe('ResetPasswordComponent', () => {
+  let component: ResetPasswordComponent;
+  let fixture: ComponentFixture<ResetPasswordComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA],
+      declarations: [ResetPasswordComponent],
+      imports: [ReactiveFormsModule, FormsModule],
+      providers: [FormBuilder],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ResetPasswordComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/shared/components/input/input.component.spec.ts
+++ src/app/shared/components/input/input.component.spec.ts
@@ -1,3 +1,13 @@
+import { InputComponent } from './input.component';
+
+describe('InputComponent', () => {
+  const component: InputComponent = new InputComponent();
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
 // import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 // import { ComponentFixture, TestBed } from '@angular/core/testing';
 // import { FormsModule, ReactiveFormsModule, FormControl } from '@angular/forms';
