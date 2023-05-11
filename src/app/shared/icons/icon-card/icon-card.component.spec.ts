d7318d072a3daa70af2289b9f2fb9bd36b66ca04
Stealves <stealves@outlook.com>
2023-05-11 17:42:32 +0900

    feat: card emit event on icon click

---
+++ src/app/shared/icons/icon-card/icon-card.component.spec.ts
@@ -0,0 +1,9 @@
+import { IconCardComponent } from './icon-card.component';
+
+describe('IconCardComponent', () => {
+  const component: IconCardComponent = new IconCardComponent();
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
