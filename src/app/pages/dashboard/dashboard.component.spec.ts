7f81b2210086ca4d26d8cfb89e8b28d9d8a4e192
Stealves <stealves@outlook.com>
2023-06-13 18:09:27 -0300

    fix: create test for dashboard

--- src/app/pages/dashboard/dashboard.component.spec.ts
+++ src/app/pages/dashboard/dashboard.component.spec.ts
@@ -1,10 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DashboardComponent } from './dashboard.component';
+import { RouterOutlet } from '@angular/router';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
+  let fixture: ComponentFixture<DashboardComponent>;
 
-  beforeEach(() => {
-    component = new DashboardComponent();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA],
+      declarations: [ DashboardComponent ],
+      imports: [RouterOutlet],
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
