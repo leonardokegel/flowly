d54fc2a387a54dfaeac5d80e253d42c4b55dad7b
Stealves <stealves@outlook.com>
2023-07-03 10:44:05 -0300

    feat:create client modal

--- src/app/shared/components/create-cliente/create-cliente.component.ts
+++ src/app/shared/components/create-cliente/create-cliente.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MODAL_DATA } from '@shared/modal/modal-tokens';
+import { ModalRef } from '@shared/modal/modal.ref';
 
 @Component({
   selector: 'app-create-cliente',
@@ -8,10 +10,17 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class CreateClienteComponent {
 
-  constructor( private formBuilder: FormBuilder ) {}
+  constructor( 
+    private formBuilder: FormBuilder,
+    private modalRef: ModalRef, 
+    // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+    @Inject(MODAL_DATA) public data: any
+    ) {}
 
   createForm: FormGroup = this.formBuilder.group({
+    empresa: ['', [Validators.required]],
     nome: ['', [Validators.required]],
+    telefone: ['', [Validators.pattern('[- +()0-9]{11}')]],
     email: ['', Validators.compose([Validators.required, Validators.email])],
   });
 
@@ -19,7 +28,17 @@ export class CreateClienteComponent {
 
   createCliente() {
     if (this.createForm?.invalid) return;
-
     console.log(this.createForm.value);
+    setTimeout(() => {
+      this.modalRef.close();
+    }, 500);
+  }
+
+  cancel() {
+    this.createForm.reset();
+    console.log(this.createForm.value);
+    setTimeout(() => {
+      this.modalRef.close();
+    }, 200);
   }
 }
