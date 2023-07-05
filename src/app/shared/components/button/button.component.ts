2ec54a92a63e6dbfb46b063d0f1eb4687d5c949a
Stealves <stealves@outlook.com>
2023-07-05 11:30:37 -0300

    fix:dynamic styles

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -22,15 +22,19 @@ export class ButtonComponent implements OnInit {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   clickButton = new EventEmitter<any>();
 
-  buttonAlternate = '';
+  buttonColor = '';
 
   ngOnInit(): void {
-    if (this.colorBg === 'ghost') {
-      this.buttonAlternate = 'text-txt-default bg-transparent hover:bg-basic-300 focus:bg-basic-300 active:bg-basic-800';
+    const colorVariants = {
+      primary: 'bg-primary-600 hover:bg-primary-700 focus:bg-primary-700 active:bg-primary-800',
+      danger: 'bg-danger-600 hover:bg-danger-700 focus:bg-danger-700 active:bg-danger-800',
+      basic: 'bg-basic-600 hover:bg-basic-700 focus:bg-basic-700 active:bg-basic-800',
+      warning: 'bg-warning-600 hover:bg-warning-700 focus:bg-warning-700 active:bg-warning-800',
+      success: 'bg-success-600 hover:bg-success-700 focus:bg-success-700 active:bg-success-800',
+      icon: 'text-txt-hint bg-transparent-600 hover:bg-basic-300',
+      ghost: 'text-txt-default bg-transparent-600 hover:bg-basic-300 focus:bg-basic-300 active:bg-basic-400 hover:text-txt-accent',
     }
 
-    if (this.colorBg === 'icon') {
-      this.buttonAlternate = 'flex p-2 rounded-full bg-transparent hover:bg-basic-300 text-txt-hint hover:text-txt-accent';
-    }
+    this.buttonColor = colorVariants[this.colorBg];
   }
 }
